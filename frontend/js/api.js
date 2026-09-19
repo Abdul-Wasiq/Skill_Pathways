f9bd313043b883f8387a17060edf1ae3fe2a683f
Wasiq <abdulwasiq651@gmail.com>
2026-09-19 20:10:27 +0500

    Update tunnel URL

--- frontend/js/api.js
+++ frontend/js/api.js
@@ -9,7 +9,7 @@
 const API_BASE_URL =
   location.hostname === "localhost" || location.hostname === "127.0.0.1"
     ? "http://127.0.0.1:8000"
-    : "https://configure-julian-mitchell-points.trycloudflare.com";
+    : "https://months-maryland-fixtures-cornell.trycloudflare.com";
 
 /**
  * Escape user-generated text before putting it into innerHTML.
